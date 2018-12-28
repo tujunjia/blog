844aae26072d2c70725c749cf89ab5490647749c
tjj955 <273288259@qq.com>
2018-12-28 12:29:19 +0800

    界面继续完善

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,4 +9,36 @@ class Index extends Controller
     {
         return $this->fetch();
     }
+
+    /**
+     * html页面的渲染
+     */
+    public function html()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * javascript页面的渲染
+     */
+    public function javascript()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * linux的渲染页面
+     */
+    public function linux()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * php的渲染页面
+     */
+    public function php()
+    {
+        return $this->fetch();
+    }
 }
